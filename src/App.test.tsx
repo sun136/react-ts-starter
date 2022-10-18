60613e5329a7d28cc56c840cc473ff744acda7d2
shx1368 <shx09@139.com>
2022-10-18 16:55:46 +0800

    test: modify the test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders btn anymock', () => {
   render(<App />);
   const linkElement = screen.getByText(/程获取anymock用户信息/i);
   expect(linkElement).toBeInTheDocument();
