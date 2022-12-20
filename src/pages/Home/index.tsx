7ee68a175ddf51581055551319e6b1dfc5480244
sun136 <shx09@139.com>
2022-12-20 23:43:28 +0800

    feat: updated example

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { useImmer } from 'use-immer';
+
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { getUserAsync } from '@/store/slice/user';
 
@@ -5,7 +7,14 @@ import styles from './index.module.css';
 
 const Home = () => {
   const dispatch = useAppDispatch();
-  const user = useAppSelector((_) => _.user);
+  const [immer, updateImmer] = useImmer({ titile: 'Welcome!' });
+  const { userInfo, status } = useAppSelector((_) => _.user);
+
+  const handleUpdateTitle = () => {
+    updateImmer((state) => {
+      state.titile = 'You changed it';
+    });
+  };
 
   const handleGetUser = () => {
     dispatch(getUserAsync());
@@ -14,15 +23,17 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <button
-          className={styles.button}
-          disabled={user.status === 'loading'}
-          onClick={handleGetUser}
-        >
-          远程获取anymock用户信息
+        <button className={styles.button} onClick={handleUpdateTitle}>
+          use immer
         </button>
-        <p>{user.status === 'failed' && '请求错误'}</p>
-        <code lang="json">{JSON.stringify(user.userInfo)}</code>
+        <p>{immer.titile}</p>
+      </div>
+      <div className={styles.box}>
+        <button className={styles.button} disabled={status === 'loading'} onClick={handleGetUser}>
+          send request{status === 'loading' ? '...' : ''}
+        </button>
+        <p>{status === 'failed' && '请求错误'}</p>
+        <code lang="json">{JSON.stringify(userInfo)}</code>
       </div>
     </div>
   );
